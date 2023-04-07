21b9669d61494083cb2fdfc5c6117035dfe52f07
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2023-04-07 15:16:56 -0300

    Adicionando a centésima trigésima terceira questão

--- questão 133.c
+++ questão 133.c
@@ -11,15 +11,25 @@ d. Imprima a matriz modificada.
 int main() {
     float matriz[3][6];
     int i, j;
-    float soma_impares = 0;
-    float media_colunas_2_4 = 0;
+    float soma_impares = 0.2f;
+    float media_colunas_2_4 = 0.2f;
 
     // Aqui é realizado a leitura dos valores da matriz.
     printf("Digite os valores da matriz:\n");
+
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 6; j++) {
             scanf("%f", &matriz[i][j]);
         }
+    }
+
+    //Aqui é realizado a impressão da matriz original para que o usuário verifique a matriz inserida.
+    printf("\nMatriz original:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 6; j++) {
+            printf("%.f ", matriz[i][j]);
+        }
+        printf("\n");
     }
 
     // Aqui é realizado a soma dos valores das colunas ímpares.
@@ -28,14 +38,14 @@ int main() {
             soma_impares += matriz[i][j];
         }
     }
-    printf("A soma dos elementos das colunas ímpares é: %.2f\n", soma_impares);
+    printf("\nA soma dos elementos das colunas ímpares é: %.f\n", soma_impares);
 
     // Aqui é realizado o cálculo da média aritmética dos elementos da segunda e quarta colunas.
     for (i = 0; i < 3; i++) {
         media_colunas_2_4 += matriz[i][1] + matriz[i][3];
     }
     media_colunas_2_4 /= 6;
-    printf("A média aritmética dos elementos das colunas 2ª e 4ª é: %.2f\n", media_colunas_2_4);
+    printf("\nA média aritmética dos elementos das colunas 2ª e 4ª é: %.f\n", media_colunas_2_4);
 
     // Aqui é realizado a substituição dos valores da sexta coluna pela soma da primeira e segunda colunas.
     for (i = 0; i < 3; i++) {
@@ -43,10 +53,10 @@ int main() {
     }
 
     // Aqui é realizado a impressão para o usuário da matriz modificada como solicitado na questão.
-    printf("Matriz modificada:\n");
+    printf("\nMatriz modificada:\n");
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 6; j++) {
-            printf("%.2f ", matriz[i][j]);
+            printf("%.f ", matriz[i][j]);
         }
         printf("\n");
     }
